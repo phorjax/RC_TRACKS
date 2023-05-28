eaf739d4239ce0351f2e3c1d57ea892e7d3d9cdb
anthony gomez <joseph.12082@gmail.com>
2023-05-27 22:09:17 -0400

    textbox added and modified, navbar modified, new section pistas added, footer added and styled

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,5 +19,6 @@ export default function RootLayout({ children }) {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
       </body>
     </html>
+    
   )
 }
